d42e915c0fe06193bfa6a235d50967e8edf74e7a
maneesh179199 <57223088+maneesh179199@users.noreply.github.com>
2020-08-27 21:58:10 +0530

    Changes

--- includes/class.database.php
+++ includes/class.database.php
@@ -32,7 +32,23 @@ $db = new ezSQL_mysqli($db_user,$db_password,$db_name,$db_host);
 	    return $res;
 
 	}
-     function login(){
+     function login($email,$password){
+     	$db_user = 'root';			//DB USERNAME
+$db_password = '';			//DB PASSWORD 
+$db_name = 'employee';		//DB NAME
+$db_host = 'localhost';		//DB SERVER
+// Include the class:
+include_once "ezsql/shared/ez_sql_core.php";
+include_once "ezsql/mysqli/ez_sql_mysqli.php";
+$db = new ezSQL_mysqli($db_user,$db_password,$db_name,$db_host);
+     	$hashPassword=hashPassword($password);
+     	$row = $db->get_row("SELECT * FROM tbluser   WHERE email='".$email."' AND password='".$hashPassword."' AND status=1 ");
+
+            if($row) {
+             return $row;   
+            }else{
+            	return FALSE;
+            }    
      	
      }
 }
